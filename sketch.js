168894c24b90c58cd20b2d66aa8ed2e599ba5a6b
islammohsen <islammohsen2@hotmail.com>
2019-01-15 01:55:29 +0200

    updated code style

--- sketch.js
+++ sketch.js
@@ -1,33 +1,8 @@
-var rotate;
-var branch;
 var dif = 60;
 
 function setup() {
 	createCanvas(600, 600);
-	rotate = function(x, y, cx, cy, angle) {
-		var ang = radians(angle);
-		var ret = {
-			x: x,
-			y: y
-		};
-		ret.x = (x - cx) * cos(ang) - (y - cy) * sin(ang) + cx;
-		ret.y = (x - cx) * sin(ang) + (y - cy) * cos(ang) + cy;
-		return ret;
-	};
-	branch = function(x, y, len, angle) {
-		if (len > 3) {
-			var end = rotate(x + len, y, x, y, angle);
-			stroke(255);
-			line(x, y, end.x, end.y);
-			
-			branch(end.x, end.y, len * 0.67, angle + dif);
-			branch(end.x, end.y, len * 0.67, angle - dif);
-		}
-		else{
-			stroke(0, 255, 0)
-			ellipse(x, y, 2, 4);
-		}
-	}
+	test();
 }
 
 function draw() {
@@ -38,8 +13,41 @@ function draw() {
 
 //takes user input
 function input(){
+	dif += 0.5;
 	if(keyIsDown(LEFT_ARROW))
 		dif -= 0.5;
 	if(keyIsDown(RIGHT_ARROW))
 		dif += 0.5;
 }
+
+function test(){
+	print("Hello");
+}
+
+function branch(x, y, len, angle)
+{
+	if (len > 3) {
+		var end = branchRotate(x + len, y, x, y, angle);
+		stroke(255);
+		line(x, y, end.x, end.y);
+		
+		branch(end.x, end.y, len * 0.67, angle + dif);
+		branch(end.x, end.y, len * 0.67, angle - dif);
+	}
+	else{
+		stroke(0, 255, 0)
+		ellipse(x, y, 2, 4);
+	}
+}
+
+function branchRotate(x, y, cx, cy, angle) {
+	var ang = radians(angle);
+	var ret = {
+		x: x,
+		y: y
+	};
+	ret.x = (x - cx) * cos(ang) - (y - cy) * sin(ang) + cx;
+	ret.y = (x - cx) * sin(ang) + (y - cy) * cos(ang) + cy;
+	return ret; 
+};
+
